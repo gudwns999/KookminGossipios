ab3f529c344a7862e5672a06f92ff620790bbc4d
yungJun Kim H <kim@hyungjunui-macbook-air.local>
2016-12-04 16:57:02 +0900

    Initial Commit

---
+++ KookminGossip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KookminGossip
+//
+//  Created by HyungJun Kim on 2016. 12. 4..
+//  Copyright © 2016년 HyungJun Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
